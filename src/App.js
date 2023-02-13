c49a06b2048938220e372e1f33de8a67947fa04f
dormirMais <soonhak1212@naver.com>
2023-02-13 13:20:00 +0900

    [Add] 기본적인 brower router경로 설정완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Navbar from "./components/Navbar";
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return <>ru working?</>;
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
