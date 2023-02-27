37c5915882cd35c769713d4f4c9b9a271014e0b3
dormirMais <soonhak1212@naver.com>
2023-02-27 12:35:39 +0900

    [CSS] Navbar CSS 정리

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { FiShoppingBag } from "react-icons/fi";
 import { BsFillPencilFill } from "react-icons/bs";
 import { login, logout, onUserStateChange } from "../api.js/firbase.js";
 import User from "./User.jsx";
+import Button from "./ui/Button.jsx";
 
 export default function Navbar() {
   const [user, setUser] = useState(null);
@@ -20,7 +21,6 @@ export default function Navbar() {
   const handleLogout = () => {
     logout();
   };
-  console.log("page refresh");
 
   return (
     <header className="flex justify-between border-b-4  border-gray-300 pb-2">
@@ -31,12 +31,14 @@ export default function Navbar() {
       <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">Products</Link>
         <Link to="/carts">Carts</Link>
-        <Link to="/products/new" className="text-2xl">
-          <BsFillPencilFill />
-        </Link>
+        {user && user.isAdmin && (
+          <Link to="/products/new" className="text-2xl">
+            <BsFillPencilFill />
+          </Link>
+        )}
         {user && <User user={user} />}
-        {!user && <button onClick={handleLogin}>Login</button>}
-        {user && <button onClick={handleLogout}>Logout</button>}
+        {!user && <Button text={"Login"} onClick={handleLogin} />}
+        {user && <Button text={"Logout"} onClick={handleLogout} />}
       </nav>
     </header>
   );
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function User({ user: { photoURL, displayName } }) {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center shrink-0">
       <img className="w-10 h-10 rounded-full" src={photoURL} alt={displayName} referrerPolicy="no-referrer" />
-      <span className="hidden md:block">{displayName}</span>
+      <span className="hidden md:block ml-2">{displayName}</span>
     </div>
   );
 }
