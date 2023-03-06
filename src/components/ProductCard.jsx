f5a715168919a3e88f9bfc32851c3722a6db4c52
dormirMais <soonhak1212@naver.com>
2023-03-06 09:29:30 +0900

    [CSS] 배너추가, item CSS적용

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function ProductCard({ product: { id, image, title, category, price } }) {
   return (
-    <li>
-      <img src={image} alt={title}></img>
-      <div>
-        <h3>{title}</h3>
+    <li className="rounded-lg shadow-m overflow-hidden cursor-pointer">
+      <img className="w-full" src={image} alt={title}></img>
+      <div className="mt-2 px-2 text-lg ">
+        <h3 className="truncate">{title}</h3>
         <p>{`$price`}</p>
-        <p>{category}</p>
+        <p className="mb-2 text-gray-500">{category}</p>
       </div>
     </li>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import Products from "../Products";
+import Banner from "../components/Banner";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Banner />
+      <Products />
+    </>
+  );
 }
